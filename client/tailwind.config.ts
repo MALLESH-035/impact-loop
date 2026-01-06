96505a54e8b2457d12534cecf533e21ea3d6ece6
MALLESH-035 <im.malleshh@gmail.com>
2026-01-06 06:31:18 +0530

    Create tailwind.config.ts

---
+++ client/tailwind.config.ts
@@ -0,0 +1,8 @@
+  },
+      },
+      fontFamily: {
+        sans: ["'Inter'", "sans-serif"],
+        display: ["'Poppins'", "sans-serif"],
+        serif: ["var(--font-serif)"],
+        mono: ["var(--font-mono)"],
+      },
